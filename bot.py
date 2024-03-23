64297b26d808f7fc939d5ccb088480ebf3a1a793
snazzawolf <ritchieyapp@yahoo.com>
2024-03-23 12:12:16 +0800

    start off

--- bot.py
+++ bot.py
@@ -1,4 +1,54 @@
 import discord
+import datetime
 import dotenv
+import os
+import threading
+from requests import get
+from json import loads
+import asyncio
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = discord.app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        MY_GUILD = discord.Object(id=1093515712900902912)
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+    
 
 dotenv.load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+CHANNEL = client.get_channel(1216427363345371246)
+
+def timestamp() -> str:
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+@client.event
+async def on_ready():\
+    print(f'{timestamp()}: Logged in as {client.user} (ID: {client.user.id})')
+
+async def cat_fact():
+    await asyncio.sleep(1)
+    try:
+        catFact = loads(get("https://catfact.ninja/fact").content.decode("utf-8"))["fact"]
+    except Exception as e:
+        catFact = f"Meowerror: {e}"
+    await CHANNEL.send(content=cat_fact)
+
+@client.tree.command(description='Manually invoke')
+@discord.app_commands.describe()
+async def silly_embed(interaction: discord.Interaction):
+    await cat_fact()
+
+async def run_blocking_task_in_thread():
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, cat_fact)
+
+if __name__ == "__main__":
+    td = threading.Thread(target=asyncio.run, args=(run_blocking_task_in_thread(),))
+    td.start()
+    client.run(os.getenv('TOKEN'))
